cf782f0af07da64161f2ed92517490428a7adf37
nurik0708 <aaikossya09@gmail.com>
2023-11-19 16:13:25 +0600

    Добавьте файлы проекта.

---
+++ Modul11/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul11
+{
+    public enum Vacancies
+    {
+        Manager,
+        Developer,
+        Designer
+    }
+
+    struct Employee
+    {
+        public string Name;
+        public Vacancies Vacancy;
+        public int[] EDate;
+        public int Salary;
+    }
+
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+            Employee employee;
+            employee.Name = "Aigerim";
+            employee.Vacancy = Vacancies.Developer;
+            employee.EDate = new int[] { 2023, 08, 27 };
+            employee.Salary = 200000;
+
+            Console.WriteLine("Name: {employee.Name}");
+            Console.WriteLine("Vacancy: {employee.Vacancy}");
+            Console.WriteLine("Hire Date: {employee.EDate[0]}.{employee.EDate[1]}.{employee.EDate[2]}");
+            Console.WriteLine("Salary: {employee.Salary}");
+        }
+    }
+}
